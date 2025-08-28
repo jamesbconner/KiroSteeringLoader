8c2488305658d63da6e3f467afa5c699874a81f9
James <jamesbconner@users.noreply.github.com>
2025-08-28 11:37:09 -0400

    bump version to 0.0.3 and remove ES modules configuration

--- out/extension.js
+++ out/extension.js
@@ -1,7 +1,44 @@
-import * as vscode from 'vscode';
-import { SteeringTemplateProvider } from './steeringTemplateProvider';
-export function activate(context) {
-    const provider = new SteeringTemplateProvider(context);
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(require("vscode"));
+const steeringTemplateProvider_1 = require("./steeringTemplateProvider");
+function activate(context) {
+    const provider = new steeringTemplateProvider_1.SteeringTemplateProvider(context);
     vscode.window.registerTreeDataProvider('kiroSteeringLoader', provider);
     const refreshCommand = vscode.commands.registerCommand('kiroSteeringLoader.refresh', () => {
         provider.refresh();
@@ -24,5 +61,5 @@ export function activate(context) {
     });
     context.subscriptions.push(refreshCommand, loadTemplateCommand, setTemplatesPathCommand);
 }
-export function deactivate() { }
+function deactivate() { }
 //# sourceMappingURL=extension.js.map
--- out/steeringTemplateProvider.js
+++ out/steeringTemplateProvider.js
@@ -1,7 +1,43 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-export class SteeringTemplateProvider {
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SteeringTemplateProvider = void 0;
+const vscode = __importStar(require("vscode"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+class SteeringTemplateProvider {
     constructor(context) {
         this.context = context;
         this._onDidChangeTreeData = new vscode.EventEmitter();
@@ -81,6 +117,7 @@ export class SteeringTemplateProvider {
         }
     }
 }
+exports.SteeringTemplateProvider = SteeringTemplateProvider;
 class TemplateItem extends vscode.TreeItem {
     constructor(label, templatePath, collapsibleState, itemType) {
         super(label, collapsibleState);
